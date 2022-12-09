2804624bebe4632684c36ca19392318f2faf08e2
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-08 20:01:55 -0600

    created server post and get  routes, client.js functions, and client routes for post and get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,59 @@
 console.log( 'js' );
 
-$( document ).ready( function(){
+$( document ).ready(onReady);
+
+
+function onReady() {
   console.log( 'JQ' );
   // Establish Click Listeners
-  setupClickListeners()
-  // load existing koalas on page load
-  getKoalas();
-
-}); // end doc ready
+  $( '#addButton' ).on( 'click', setupClickListeners);
+  
+}; // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
+  console.log( 'in addButton on click' );
+  // get user input and put in an object
+  
+  
+  // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
-  }); 
-}
+  }; 
 
+
+getKoalas();
+//GET Koala list from server
+//send to append to DOM
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then(function(response){
+    console.log('in /GET', response);
+    appendToDom(response);
+  });
   
 } // end getKoalas
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
- 
+$.ajax({
+  type: 'POST',
+  url: '/koalas',
+  data: newKoala
+}).then(function(){
+  console.log('req for newKoala');
+}).catch(error);
+  console.log(error);
 }
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+
 // DB CONNECTION
 
 
 // GET
-
+koalaRouter.get('/',(req, res)=> {
+    res.send(koalasList);
+    console.log('GET request from server');
+})
 
 // POST
-
+koalaRouter.post('/', (req, res) =>{
+    res.send(req.body);
+    console.log('POST was made');
+})
 
 // PUT
 
