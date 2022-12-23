852a9982378851f016bb22e2f1d6a6a8cc7bfa58
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-22 19:06:15 -0600

    added form validation to check for any empty inputs values

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -26,116 +26,153 @@ function setupClickListeners() {
       readyToTransfer: $('#readyToTransferIn').val(),
       notes: $('#notesIn').val(),
     };
+
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
-  }; 
-
-
-//GET Koala list from server
-//send to append to DOM
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  $.ajax({
-    type: 'GET',
-    url: '/koalas'
-  }).then(function(response){
-    console.log('in /GET', response);
-    appendToDom(response);
-  });
+  }
   
-} // end getKoalas
-
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
-$.ajax({
-  type: 'POST',
-  url: '/koalas',
-  data: newKoala //req.body is everything in ajax post request
-}).then(function(){
-  console.log('req for newKoala');
-  getKoalas();
-// }).catch(error);
-//   console.log(error);
-})
-}
-
-function appendToDom(array){
-  $('#viewKoalas').empty();
-  // for (let koala of array){
-    for (let i=0; i < array.length; i++) {
-    let id = array[i].id;
-    let transfer = array[i].ready_to_transfer;
-    console.log(`this is our current id: ${id}`);
-    $('#viewKoalas').append(`
-    <tr>
-      <td>
-        ${array[i].name}      
-      </td>
-      <td>
-        ${array[i].age}      
-      </td>
-      <td>
-        ${array[i].gender}      
-      </td>
-      <td>
-        ${array[i].ready_to_transfer}      
-      </td>
-      <td>
-        ${array[i].notes}      
-      </td>
-      <td>
-        <button class="transfer-yes" data-transfer=${transfer} data-id=${id}>Ready To Transfer</button>
-      </td>
-      <td>
-        <button class="delete" data-id=${id}>Delete</button>
-      </td>
-    </tr>
-  `)};
-};
-
-function readyToTransfer() {
-  console.log('readyToTransfer');
   
-  const id = $(this).data('id');
-  const transfer = $(this).data('transfer');
-
-  console.log(id);
-  console.log(transfer);
-
-  $.ajax({
-    type: 'PUT',
-    url: `/koalas/ready_to_transfer/${id}`,
-    data: {ready_to_transfer: `${transfer}`}
-  }).then(function() {
-    getKoalas();
-  }).catch(function(error) {
-    console.log('error with putting', error);
-  })
-}
-
-function deleteKoala(){
-  const id = $(this).data('id');
   
-  swal({
-    title: 'Are you sure you want to delete this koala?',
-    icon: 'warning',
-    buttons: true,
-    dangerMode: true
-  })
-  .then((willDelete) =>{
-    if (willDelete) {
-      $.ajax({
-        type: 'DELETE',
-        url: `/koalas/${id}`
-      }).then(function(){
-        getKoalas();
-      }).catch(function(error){
-        console.log('error with deleting, ', error);
-      })
-    } else {
+  //GET Koala list from server
+  //send to append to DOM
+  function getKoalas(){
+    console.log( 'in getKoalas' );
+    // ajax call to server to get koalas
+    $.ajax({
+      type: 'GET',
+      url: '/koalas'
+    }).then(function(response){
+      console.log('in /GET', response);
+      appendToDom(response);
+    });
+    
+  } // end getKoalas
+  
+  function saveKoala( newKoala ){
+    console.log( 'in saveKoala', newKoala );
+    if (isInvalid()) {
       return;
-    }   
-  })
-}
+    }
+    // ajax call to server to get koalas
+    $.ajax({
+      type: 'POST',
+      url: '/koalas',
+      data: newKoala //req.body is everything in ajax post request
+    }).then(function(){
+      console.log('req for newKoala');
+      getKoalas();
+      // }).catch(error);
+      //   console.log(error);
+    })
+  }
+  
+  function appendToDom(array){
+    $('#viewKoalas').empty();
+    // for (let koala of array){
+      for (let i=0; i < array.length; i++) {
+        let id = array[i].id;
+        let transfer = array[i].ready_to_transfer;
+        console.log(`this is our current id: ${id}`);
+        $('#viewKoalas').append(`
+        <tr>
+        <td>
+        ${array[i].name}      
+        </td>
+        <td>
+        ${array[i].age}      
+        </td>
+        <td>
+        ${array[i].gender}      
+        </td>
+        <td>
+        ${array[i].ready_to_transfer}      
+        </td>
+        <td>
+        ${array[i].notes}      
+        </td>
+        <td>
+        <button class="transfer-yes" data-transfer=${transfer} data-id=${id}>Ready To Transfer</button>
+        </td>
+        <td>
+        <button class="delete" data-id=${id}>Delete</button>
+        </td>
+        </tr>
+        `)};
+      };
+      
+      function readyToTransfer() {
+        console.log('readyToTransfer');
+        
+        const id = $(this).data('id');
+        const transfer = $(this).data('transfer');
+        
+        console.log(id);
+        console.log(transfer);
+        
+        $.ajax({
+          type: 'PUT',
+          url: `/koalas/ready_to_transfer/${id}`,
+          data: {ready_to_transfer: `${transfer}`}
+        }).then(function() {
+          getKoalas();
+        }).catch(function(error) {
+          console.log('error with putting', error);
+        })
+      }
+      
+      function deleteKoala(){
+        const id = $(this).data('id');
+        
+        swal({
+          title: 'Are you sure you want to delete this koala?',
+          icon: 'warning',
+          buttons: true,
+          dangerMode: true
+        })
+        .then((willDelete) =>{
+          if (willDelete) {
+            $.ajax({
+              type: 'DELETE',
+              url: `/koalas/${id}`
+            }).then(function(){
+              getKoalas();
+            }).catch(function(error){
+              console.log('error with deleting, ', error);
+            })
+          } else {
+            return;
+          }
+        })
+      }
+
+      function isInvalid() {
+        console.log('in check validity function');
+
+        let koalaName = $('#nameIn').val();
+        let koalaAge = $('#ageIn').val();
+        let koalaGender = $('#genderIn').val();
+        let readyToTransfer = $('#readyToTransferIn').val();
+        let notes = $('#notesIn').val();
+    
+        if (koalaName == '') {
+          alert ('name cannot be blank');
+          return true;
+        }
+        if (koalaAge =='') {
+          alert ('age cannot be blank');
+          return true;
+        } 
+        if (koalaGender == '') {
+          alert ('gender cannot be blank');
+          return true;
+        }
+        if (readyToTransfer == '') {
+          alert ('readyToTransfer cannot be blank');
+          return true;
+        }
+        if (notes == '') {
+          alert ('notes cannot be blank');
+          return true;
+        }
+        return false;    
+    }
